0a4d1b8327d2d60ccc57527cd6dda2a2bbdf76c8
Aleksey <aleksey91scorp@bk.ru>
2020-05-27 22:21:16 +0300

    search, load and render data from server added

--- script/main.js
+++ script/main.js
@@ -1,11 +1,25 @@
 //Constants
 const IMG_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
+const SERVER = "https://api.themoviedb.org/3";
+const API_KEY = "08f4b8dae8c78a5afed1fc1dd61130f4";
 
 //Menu
 const leftMenu = document.querySelector(".left-menu"),
   hamburger = document.querySelector(".hamburger"),
   tvShowsList = document.querySelector(".tv-shows__list"),
-  modal = document.querySelector(".modal");
+  modal = document.querySelector(".modal"),
+  tvShows = document.querySelector(".tv-shows"),
+  tvCardImg = document.querySelector(".tv-card__img"),
+  modalTitle = document.querySelector(".modal__title"),
+  genresList = document.querySelector(".genres-list"),
+  rating = document.querySelector(".rating"),
+  description = document.querySelector(".description"),
+  modalLink = document.querySelector(".modal__link"),
+  searchForm = document.querySelector(".search__form"),
+  searchFromInput = document.querySelector(".search__form-input");
+
+const loading = document.createElement("div");
+loading.className = "loading";
 
 //Open/close menu
 hamburger.addEventListener("click", () => {
@@ -21,6 +35,7 @@ document.addEventListener("click", (event) => {
 });
 
 leftMenu.addEventListener("click", () => {
+  event.preventDefault();
   const target = event.target;
   const dropdown = target.closest(".dropdown");
   if (dropdown) {
@@ -38,8 +53,23 @@ tvShowsList.addEventListener("click", (event) => {
   const card = target.closest(".tv-card");
 
   if (card) {
-    document.body.style.overflow = "hidden";
-    modal.classList.remove("hide");
+    new DBService()
+      .getTvShow(card.id)
+      .then((response) => {
+        tvCardImg.src = IMG_URL + response.poster_path;
+        tvCardImg.alt = response.name;
+        modalTitle.textContent = response.name;
+        genresList.innerHTML = response.genres.reduce((acc, item) => {
+          return `${acc}<li>${item.name}</li>`;
+        }, "");
+        rating.textContent = response.vote_average;
+        description.textContent = response.overview;
+        modalLink.href = response.homepage;
+      })
+      .then(() => {
+        document.body.style.overflow = "hidden";
+        modal.classList.remove("hide");
+      });
   }
 });
 
@@ -85,6 +115,20 @@ const DBService = class {
   getTestData = () => {
     return this.getData("test.json");
   };
+
+  getTestCard = () => {
+    return this.getData("card.json");
+  };
+
+  getSearchResult = (query) => {
+    return this.getData(
+      `${SERVER}/search/tv?api_key=${API_KEY}&language=ru-RU&query=${query}&include_adult=false`
+    );
+  };
+
+  getTvShow = (id) => {
+    return this.getData(`${SERVER}/tv/${id}?api_key=${API_KEY}&language=ru-RU`);
+  };
 };
 
 const renderCard = (response) => {
@@ -95,16 +139,18 @@ const renderCard = (response) => {
       name: title,
       poster_path: poster,
       vote_average: vote,
+      id,
     } = item;
 
     const posterIMG = poster ? IMG_URL + poster : "img/no-poster.jpg";
-    const backdropIMG = backdrop ? IMG_URL + backdrop : "img/no-poster.jpg";
+    const backdropIMG = backdrop ? IMG_URL + backdrop : "";
     const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : "";
 
     const card = document.createElement("li");
+    card.idTV = id;
     card.classList.add("tv-shows__item");
     card.innerHTML = `
-      <a href="#" class="tv-card">
+      <a href="#" id="${id}" class="tv-card">
         ${voteElem}
         <img
           class="tv-card__img"
@@ -115,8 +161,17 @@ const renderCard = (response) => {
         <h4 class="tv-card__head">${title}</h4>
       </a>
     `;
+    loading.remove();
     tvShowsList.append(card);
   });
 };
 
-new DBService().getTestData().then(renderCard);
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = searchFromInput.value.trim();
+  if (value) {
+    tvShows.append(loading);
+    new DBService().getSearchResult(value).then(renderCard);
+  }
+  searchFromInput.value = "";
+});
